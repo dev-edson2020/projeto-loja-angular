14784594711a8aa8210fd42e931e8804d7c8ad36
Edson Meira <edson.meira@itixti.com.br>
2024-04-29 18:08:12 -0300

    Ajustes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import { ProductService } from '../services/product.service';
 export class HomeComponent implements OnInit {
 popularProducts: undefined | Product[];
 
+
+
   constructor(private product: ProductService) { }
 
   ngOnInit(): void {
