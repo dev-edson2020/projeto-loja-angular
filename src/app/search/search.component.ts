a4b8212210c3103082d514d609e4eb474ca1d3bf
Edson Meira <edson.meira@itixti.com.br>
2024-05-04 06:51:05 -0300

    Ajustes navbar

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../data-types';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  searchResult: undefined | Product[];
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute, private product: ProductService) { }
 
   ngOnInit(): void {
-  }
+    const query = this.activeRoute.snapshot.paramMap.get('query');
+    if (query) {
+       this.product.searchProducts(query).subscribe(
+           (result) => {
+               this.searchResult = result;
+               console.log(this.searchResult);
+           },
+           (error) => {
+               console.error('Erro ao buscar produtos:', error);
+           }
+       );
+    }
+}
+
 
 }
